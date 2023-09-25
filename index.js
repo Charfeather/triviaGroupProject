eef46de42604f8bd34698c13303979f1d17ff2c3
Peyton Meredith <peymeredith@gmail.com>
2023-09-25 14:05:16 -0500

    made radio fieldsets for trivia questions

---
+++ index.js
@@ -0,0 +1,23 @@
+ 
+
+
+
+
+
+
+
+
+// const fieldsets = document.querySelectorAll(".fieldset")
+// const progressBar = document.querySelector('#progress-bar')
+// function progressBarFill(fieldset) {
+//     for fieldset in fieldsets {
+//         let width = 0
+//         if fieldset radio checked{
+//             width++
+//             progressBar.style.width = width + '%'
+//         }
+//     }
+// }
+
+
+// console.log(fieldsets)
